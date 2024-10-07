cb1f8754739894c5fe6ed75d8ec02f6678d4ecc2
maripurtskhvanidze <maripurtskhvanidze49@gmail.com>
2024-10-08 00:17:05 +0400

    first commit

---
+++ day 1/level0/homework/homework.py
@@ -0,0 +1,12 @@
+name=("mari")
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "mari" არის ცვლადისთვის მნიშვნელობა
+
+surname=("purtskhvanidze")
+
+age=(15)
+
+height=1.55
+
+print( name  +  " " +  surname  +  " " + str (age)  +  " " + str(height) )
