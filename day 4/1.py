ad94abf7fb776e6dafb902e215212182c399d00e
maripurtskhvanidze <maripurtskhvanidze49@gmail.com>
2024-10-16 19:52:25 +0400

    goa

---
+++ day 4/1.py
@@ -0,0 +1,86 @@
+name="mari" 
+surname="purtskhvanidze" 
+age=15 
+num1=7
+num2=12.7
+num3=4.5
+num4=8.9
+
+print(name)
+print(surname)
+print(age)
+print(num1)
+print(num2)
+print(num3)
+print(num4)
+
+
+print(7 + 8)
+print(6 + 5)
+print(9 + 4)
+print(8 + 9)
+print(3 + 7)
+
+print(9 - 7)
+print(8 - 4)
+print(10 - 5)
+print(6 - 2)
+print(9 - 5)
+
+print(8 * 5) 
+print(9 * 3)
+print(7 * 2)
+print(3 * 2)
+print(7 * 4)
+
+print(8 / 2)
+print(9 / 2)
+print(6 / 3)
+print(4 / 2)
+print(9 / 3)
+
+print(2 ** 2)
+print(6 ** 7)
+print(9 ** 5)
+print(6 ** 2)
+print(2 ** 2)
+
+print(7 % 3)
+print(5 % 3)
+print(7 % 5)
+print(9 % 5)
+print(7 % 4)
+
+
+print(7 + 8)
+print(6 + 5)
+print(9 + 4)
+print(8 + 9)
+print(3 + 7)
+
+print(9 - 7)
+print(8 - 4)
+print(10 - 5)
+print(6 - 2)
+print(9 - 5)
+
+print(8 * 5) 
+print(9 * 3)
+print(7 * 2)
+print(3 * 2)
+print(7 * 4)
+
+print(8 / 2)
+print(9 / 2)
+print(6 / 3)
+print(4 / 2)
+print(9 / 3)
+
+print(2 ** 2)
+print(6 ** 7)
+print(9 ** 5)
+print(6 ** 2)
+print(2 ** 2)
+
+
+print((2 + 2) * (3 - 1) * (8 / 2) * (9 * 3) * (7 % 4) * (5 ** 2))
