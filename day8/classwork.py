4a28a21b9960ee09c457a1255800027a2aca6bba
maripurtskhvanidze <maripurtskhvanidze49@gmail.com>
2024-10-30 00:37:39 +0400

    eightth commit

---
+++ day8/classwork.py
@@ -0,0 +1,16 @@
+name=  input("enter your name: ")
+surname=  input("enter your surname: ")
+age= input("enter your age: ")
+number1= input("enter number1: ")
+number2= input("enter number2: ")
+
+print(type(name))
+print(type(surname))
+print(type(age))
+print(type(number1))
+print(type(number2))
+
+
+num1= input("enter num1: ")
+num2= input("enter num2: ")
+print(num1 + num2)
---
+++ day8/homework.py
@@ -0,0 +1,17 @@
+num1=6
+num2=5
+num3=8
+num4=10
+num5=20
+
+print((num1 + num4)**num2 * (num5 - num3) / num3 % num2)
+print((num1 + num2) / num3 - (num4*num5) + num1**num3 % num2)
+print((num1 + num4)**num2 * (num5 - num3) / num3 % num2)
+print((num2 + num3) * (num5 / num2)**num4 - num3 % num4)
+print(num1 + num3 * num2  / num5 ** num4 - num2 % num1)
+print((num1 + num2) / num3 - num4*num5 + num1**num3 % num2)
+print((num1 + num2) / (num3 - num4*num5 + num4)**num5 % num1)
+print((num1 + num3)**num2 * (num4 - num3) / num3 % num5)
+print((num3 + num4)**num2 * (num5 - num2) / num3 % num2)
+print((num2 ** num3) + num4 / num1 * (num2 % num1))
+print(num5 + num2 ** num1  / num4 * num4 - num2 % num1)
