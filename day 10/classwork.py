f085c6bf8fa2bdab43c20a2a0ad1a41f843d0949
maripurtskhvanidze <maripurtskhvanidze49@gmail.com>
2024-11-08 17:26:30 +0400

    tenth commit

---
+++ day 10/classwork.py
@@ -0,0 +1,45 @@
+num1=float(input("enter number:"))
+num2=float(input("enter number:"))
+num3=float(input("enter number:"))
+
+print(type(num1))
+print(type(num2))
+print(type(num3))
+
+
+
+weight=input("enter your weight:")
+height=input("enter your height:")
+bmi=weight / height ** 2
+print(bmi)
+
+
+print(5 > 3)
+print(3 > 2)
+print(4 > 3)
+print(2 > 1)
+print(8 > 1)
+
+print( 7 < 8)
+print(10 < 2)
+print(2 < 1)
+print(90 < 99)
+print(80 < 90)
+
+print(5 >= 5)
+print(10 >= 10)
+print(9 >= 9)
+print(8 >= 8)
+print(7 >= 7)
+
+print(10 <= 10)
+print(20 <= 20)
+print(9 <= 8)
+print(7 <= 9)
+print(99 <= 99)
+
+print(7 != 9)
+print(9 != 10)
+print(8 != 10)
+print(10 != 11)
+print(12 != 13)
