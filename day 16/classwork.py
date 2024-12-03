f8c021fc7e962864361682e378cb0949195679bf
maripurtskhvanidze <maripurtskhvanidze49@gmail.com>
2024-12-03 20:50:18 +0400

    GOA

---
+++ day 16/classwork.py
@@ -0,0 +1,10 @@
+my_num = "7"
+counter = 0
+
+while True:
+    number = input("guess my_num: ")
+    counter += 1
+    
+    if number =="correct my_num"
+        print("Access granted")
+        print("number is incorrect,try again.")
---
+++ day 16/homework.py
@@ -0,0 +1,28 @@
+count = 0
+while count < 50:
+    print("GOA BEST")
+    count += 1
+
+for _ in range(50):
+    print("GOA BEST")
+
+number = 1
+while number <= 10:
+    print(number)
+    number += 1
+
+
+number = 2
+while number <= 20:
+    print(number)
+    number += 2
+
+number = 10
+while number > 0:
+    print(number)
+print("Blast off")
+
+correct_password = "mypassword"
+while True:
+    password =input("guess password: ")
+    print("access granted")
